c067167a43a9c90e06a56c64c2d248974f01e205
ryysd <ry.ysd01@gmail.com>
2015-04-27 23:43:22 +0900

    add Genre model/migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427134153) do
+ActiveRecord::Schema.define(version: 20150427144136) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "hash_tag",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "illusts", force: :cascade do |t|
     t.datetime "created_at",                  null: false
