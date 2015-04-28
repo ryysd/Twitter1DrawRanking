f1e988772532c72158e568ebb3c676418a51d788
ryysd <ry.ysd01@gmail.com>
2015-04-29 01:14:29 +0900

    fix typo

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,6 @@ class TweetsController < ApplicationController
   def debug_update
     genre = Genre.find_by_id params[:genre_id]
     Tweet.create_by_genre genre
-    render :json => "debug update"
+    render :json => "debug-update"
   end
 end
