64022015b5eba7c668a79e84a1d929af39d045c2
ryysd <ry.ysd01@gmail.com>
2015-05-09 17:24:13 +0900

    fix data collect duration

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :environment, :development
 set :output, '/usr/local/var/log/cron/cron.log'
 
-every 3.minute do
+every :hour do
   rake "one_draw:illust:fetch[2]"
   rake "one_draw:illust:update[1]"
 end
