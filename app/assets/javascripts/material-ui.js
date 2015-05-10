121f707f51b40ca77a7978d2804a1ca4509d680f
ryysd <ry.ysd01@gmail.com>
2015-05-10 15:23:06 +0900

    add react-router and sample routes

--- app/assets/javascripts/material-ui.js
+++ app/assets/javascripts/material-ui.js
@@ -1,11 +1,46 @@
-var React = require('react'),
-  mui = require('material-ui'),
-  RaisedButton = mui.RaisedButton,
-  AppBar = mui.AppBar;
+var React = require('react');
+var mui = require('material-ui');
+var RaisedButton = mui.RaisedButton;
+var AppBar = mui.AppBar;
 
-$(function(){
-  React.render(
-    <AppBar />,
-    document.body
-  );
+var Router = require('react-router'); 
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
+  render: function() {
+    return (
+        <div>
+          <AppBar/>
+          <RouteHandler/>
+        </div>
+    );
+  }
+});
+
+var Dashboard = React.createClass({
+  render: function() {
+    return (<div>aaaaaaaaaaaaaaa</div>);
+  }
+});
+
+var Test = React.createClass({
+  render: function() {
+    return (<div>bbbbbbbbbbbbbbbb</div>);
+  }
+});
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <DefaultRoute handler={Dashboard}/>
+    <Route name="test" handler={Test} />
+  </Route>
+);
+
+$(function() {
+  Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.body);
+  });
 });
